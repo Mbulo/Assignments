7d8bd82de38fce80d3ac8459579e9aa1d2dd4593
Mbulo <peter.mbulo@strathmore.edu>
2023-06-21 18:27:52 +0300

    assignment submission

---
+++ Assignment1.py
@@ -0,0 +1,23 @@
+#.ADD
+#.SUBTRACT
+#.MULTIPLY
+#.DIVIDE
+
+print("select an operation to perform:")
+print("1. ADD")
+print("2. SUBTRACT")
+print("3. MULTIPLY")
+print("4. DIVIDE")
+
+operation = input()
+
+if operation == 1
+    #code for ADD
+elif operation == 2    
+    #code for SUBTRACT
+elif operation == 3
+    #code for MULTIPLY
+elif operation == 4
+    #code for DIVIDE
+else:
+    print("invalid entry")
